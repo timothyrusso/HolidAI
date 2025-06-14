b1b3b0367ecd1f4d241962de8ec1abe45d9edb56
Timothy Russo <85668106+timothyrusso@users.noreply.github.com>
2025-06-14 15:56:51 +0300

    fix: user remove trip (#155)

--- ui/queries/trips/mutation/useAddToFavoriteTrip.ts
+++ ui/queries/trips/mutation/useAddToFavoriteTrip.ts
@@ -2,20 +2,20 @@ import { db } from '@/configs/firebaseConfig';
 import { logger } from '@/di/resolve';
 import type { UserTrips } from '@/modules/trip/domain/dto/UserTripsDTO';
 import { dbKeys } from '@/modules/trip/domain/entities/DbKeys';
-import auth from '@react-native-firebase/auth';
+import { useUser } from '@clerk/clerk-expo';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { doc, updateDoc } from 'firebase/firestore';
 import { tripsKeys } from '../TripsKeys';
 
 export const useAddToFavoriteTrip = (id: string) => {
   const queryClient = useQueryClient();
-  const user = auth().currentUser;
+  const { user } = useUser();
 
   const { mutate } = useMutation({
     mutationKey: [tripsKeys.addTripToFavorites, id],
     mutationFn: async (isFavorite: boolean) => {
       try {
-        const docRef = doc(db, `${dbKeys.userTrips}/${user?.uid}/trips`, id);
+        const docRef = doc(db, `${dbKeys.userTrips}/${user?.id}/trips`, id);
 
         await updateDoc(docRef, {
           isFavorite: !isFavorite,
