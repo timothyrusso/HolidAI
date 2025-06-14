c9ef42ec295d071f15405bc5d5ea85d536fb0e9d
timothyrusso <russotimothy@live.it>
2025-06-14 14:54:34 +0200

    fix: user remove trip

--- ui/queries/trips/mutation/useRemoveTrip.ts
+++ ui/queries/trips/mutation/useRemoveTrip.ts
@@ -1,19 +1,19 @@
 import { db } from '@/configs/firebaseConfig';
 import type { UserTrips } from '@/modules/trip/domain/dto/UserTripsDTO';
 import { dbKeys } from '@/modules/trip/domain/entities/DbKeys';
-import auth from '@react-native-firebase/auth';
+import { useUser } from '@clerk/clerk-expo';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { deleteDoc, doc } from 'firebase/firestore';
 import { tripsKeys } from '../TripsKeys';
 
 export const useRemoveTrip = (id: string) => {
   const queryClient = useQueryClient();
-  const user = auth().currentUser;
+  const { user } = useUser();
 
   const { mutate } = useMutation({
     mutationKey: [tripsKeys.removeUserTrip, id],
     mutationFn: async (id: string) => {
-      const docRef = doc(db, `${dbKeys.userTrips}/${user?.uid}/trips`, id);
+      const docRef = doc(db, `${dbKeys.userTrips}/${user?.id}/trips`, id);
       await deleteDoc(docRef);
 
       return true;
--- ui/queries/trips/query/useGenerateAiUserTripQuery.ts
+++ ui/queries/trips/query/useGenerateAiUserTripQuery.ts
@@ -1,13 +1,13 @@
 import { AiModels } from '@/configs/ai/AiModels';
 import { generatedTripSchema } from '@/modules/trip/domain/entities/GenerateTripSchema';
 import { useVercelAi } from '@/ui/hooks/useVercelAi';
-import auth from '@react-native-firebase/auth';
+import { useUser } from '@clerk/clerk-expo';
 import { useQuery } from '@tanstack/react-query';
 import { QueryOptionKeys } from '../../QueryOptionKeys';
 import { tripsKeys } from '../TripsKeys';
 
 export const useGenerateAiUserTripQuery = (prompt: string) => {
-  const user = auth().currentUser;
+  const { user } = useUser();
   const { generateAiObject } = useVercelAi();
 
   const { data, isLoading } = useQuery({
