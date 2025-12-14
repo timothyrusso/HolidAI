50613f784cd9825e546c6cb11e3d6fd0c4273336
Timothy Russo <russotimothy@live.it>
2025-12-14 15:46:38 +0100

    fix: lint

--- app.config.js
+++ app.config.js
@@ -47,7 +47,7 @@ export default ({ config }) => {
     android: {
       ...config.android,
       adaptiveIcon: {
-        backgroundColor: "#FFFFFF",
+        backgroundColor: '#FFFFFF',
       },
       edgeToEdgeEnabled: true,
       config: {
