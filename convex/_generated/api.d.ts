cae28a664ab68c342fc12784faae63135257b4ac
Timothy Russo <russotimothy@live.it>
2025-12-24 15:09:22 +0100

    feat: update convex db configuration

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,20 +8,16 @@
  * @module
  */
 
-import type * as trips from "../trips.js";
-import type * as validators_Trips from "../validators/Trips.js";
-import type * as validators_User from "../validators/User.js";
+import type * as trips from '../trips.js';
+import type * as validators_Trips from '../validators/Trips.js';
+import type * as validators_User from '../validators/User.js';
 
-import type {
-  ApiFromModules,
-  FilterApi,
-  FunctionReference,
-} from "convex/server";
+import type { ApiFromModules, FilterApi, FunctionReference } from 'convex/server';
 
 declare const fullApi: ApiFromModules<{
   trips: typeof trips;
-  "validators/Trips": typeof validators_Trips;
-  "validators/User": typeof validators_User;
+  'validators/Trips': typeof validators_Trips;
+  'validators/User': typeof validators_User;
 }>;
 
 /**
@@ -32,10 +28,7 @@ declare const fullApi: ApiFromModules<{
  * const myFunctionReference = api.myModule.myFunction;
  * ```
  */
-export declare const api: FilterApi<
-  typeof fullApi,
-  FunctionReference<any, "public">
->;
+export declare const api: FilterApi<typeof fullApi, FunctionReference<any, 'public'>>;
 
 /**
  * A utility for referencing Convex functions in your app's internal API.
@@ -45,9 +38,6 @@ export declare const api: FilterApi<
  * const myFunctionReference = internal.myModule.myFunction;
  * ```
  */
-export declare const internal: FilterApi<
-  typeof fullApi,
-  FunctionReference<any, "internal">
->;
+export declare const internal: FilterApi<typeof fullApi, FunctionReference<any, 'internal'>>;
 
 export declare const components: {};
