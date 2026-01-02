6907a3decf4ae9eb4f15e8ce62e4c42d6a2ebb23
Timothy Russo <russotimothy@live.it>
2026-01-02 12:52:49 +0100

    feat: remove unused keys

--- ui/queries/trips/TripsKeys.ts
+++
@@ -1,6 +0,0 @@
-export const tripsKeys = {
-  getUserTrips: 'get_user_trips',
-  addTripToFavorites: 'add_trip_to_favorites',
-  removeUserTrip: 'remove_user_trip',
-  generateAiUserTrip: 'generate_ai_user_trip',
-};
