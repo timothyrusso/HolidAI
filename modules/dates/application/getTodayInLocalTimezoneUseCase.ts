ae218f2d3ff54a0ca480066c47fca84b09395bbb
Timothy Russo <russotimothy@live.it>
2026-03-15 16:39:21 +0100

    feat(270): remove old dates module

--- modules/dates/application/getTodayInLocalTimezoneUseCase.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Gets the current date and time adjusted to the local timezone.
- * @example
- * const localDate = todayInLocalTimezone;
- * console.log(localDate); // Outputs the current date and time in the local timezone
- */
-export const getTodayInLocalTimezoneUseCase = new Date(Date.now() - new Date().getTimezoneOffset() * 60000);
