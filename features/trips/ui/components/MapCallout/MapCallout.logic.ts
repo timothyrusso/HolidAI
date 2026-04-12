6322654d339e45cee1ec185786c75a73dd29c4c4
Timothy Russo <russotimothy@live.it>
2026-04-12 19:33:37 +0200

    fix(311): use correct method to navigate to the map

--- features/trips/ui/components/MapCallout/MapCallout.logic.ts
+++ features/trips/ui/components/MapCallout/MapCallout.logic.ts
@@ -1,19 +1,8 @@
-import { Linking, Platform } from 'react-native';
+import { showLocation } from 'react-native-map-link';
 
 export const useMapCalloutLogic = () => {
   const handleOpenMaps = (latitude: number, longitude: number, title: string) => {
-    const scheme = Platform.select({
-      ios: 'maps:',
-      android: 'geo:',
-    });
-    const url = Platform.select({
-      ios: `${scheme}?q=${encodeURIComponent(title)}&ll=${latitude},${longitude}`,
-      android: `${scheme}?q=${latitude},${longitude}(${encodeURIComponent(title)})`,
-    });
-
-    if (url) {
-      Linking.openURL(url);
-    }
+    showLocation({ latitude, longitude, title });
   };
 
   return {
