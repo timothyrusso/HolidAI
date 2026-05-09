ec3fd217c39dd6f621f589043405d0e1b2374dcd
Timothy Russo <russotimothy@live.it>
2026-05-09 14:59:00 +0200

    feat(330): use proper spacing

--- ui/components/providers/ToastProvider.tsx
+++ ui/components/providers/ToastProvider.tsx
@@ -1,7 +1,8 @@
+import { spacing } from '@/ui/style/dimensions/spacing';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Toast from 'react-native-toast-message';
 
 export const ToastProvider = () => {
   const { top } = useSafeAreaInsets();
-  return <Toast topOffset={top + 8} />;
+  return <Toast topOffset={top + spacing.SingleAndHalf} />;
 };
