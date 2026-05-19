ea89102377bb145a53cedca56c6e2e7441d48e81
Timothy Russo <russotimothy@live.it>
2026-05-19 17:13:43 +0200

    fix(267): wrong header for authentication flow

--- app/(main)/(not_authenticated)/_layout.tsx
+++ app/(main)/(not_authenticated)/_layout.tsx
@@ -1,17 +1,11 @@
 import { Routes } from '@/features/core/navigation';
-import { CustomHeader } from '@/features/core/ui';
 import { Stack } from 'expo-router';
 
 export default function LoginLayout() {
   return (
     <Stack>
       <Stack.Screen name={Routes.Welcome} options={{ headerShown: false }} />
-      <Stack.Screen
-        name={Routes.SignInOrSignUp}
-        options={{
-          header: () => <CustomHeader title="SIGNIN.TITLE" />,
-        }}
-      />
+      <Stack.Screen name={Routes.SignInOrSignUp} options={{ headerShown: false }} />
     </Stack>
   );
 }
