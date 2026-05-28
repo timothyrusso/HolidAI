a12f94cb07258be928ebd1917e86c1632db23f8c
Timothy Russo <russotimothy@live.it>
2026-05-28 10:52:01 +0200

    feat(310): update food fallback image

--- features/trips/ui/components/FoodCard/components/DishItem/DishItem.tsx
+++ features/trips/ui/components/FoodCard/components/DishItem/DishItem.tsx
@@ -5,8 +5,6 @@ import { styles } from '@/features/trips/ui/components/FoodCard/components/DishI
 import type { FC } from 'react';
 import { View } from 'react-native';
 
-const fallback = require('@/features/core/ui/assets/images/no-image-placeholder.jpg');
-
 type DishItemProps = { dish: TypicalDish };
 
 export const DishItem: FC<DishItemProps> = ({ dish }) => {
@@ -17,7 +15,7 @@ export const DishItem: FC<DishItemProps> = ({ dish }) => {
       {isLoading ? (
         <BaseSkeleton style={styles.skeleton} />
       ) : (
-        <CustomImage source={typeof image === 'string' ? { uri: image } : fallback} style={styles.image} />
+        <CustomImage source={typeof image === 'string' ? { uri: image } : image} style={styles.image} />
       )}
       <CustomText text={dish.name} style={styles.label} numberOfLines={1} ellipsizeMode="tail" />
     </View>
